3ae484472621e525c42c3b1988520ffbb49c64e8
MisterXav <valckbretonxavier@gmail.com>
2025-02-03 19:19:35 +0100

    feat: Remove unused Discord link and update mail

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -29,7 +29,7 @@ import NavBar from '../components/NavBar.vue'
 
         <!-- Mail -->
         <div class="transition-all duration-300 ease-in-out hover:scale-110 hover:ring-2 hover:ring-blue-500 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="mailto:xavier@misterxav.fr" target="_blank">
+            <a href="mailto:xvalckbreton@proton.me" target="_blank">
                 <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Mail</h5>
             </a>
             <p class="mb-3 font-normal text-gray-500 dark:text-gray-300">Vous pouvez m'envoyer un mail</p>
@@ -41,21 +41,5 @@ import NavBar from '../components/NavBar.vue'
             </a>
         </div>
 
-    </div>
-
-    <div class="grid sm:place-items-center py-4 sm:px-2 mx-auto max-w-screen-xl lg:py-8">
-        <!-- Discord -->
-        <div class="transition-all duration-300 ease-in-out hover:scale-110 hover:ring-2 hover:ring-indigo-400 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://discord.com" target="_blank">
-                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Discord</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-300">Vous pouvez me retrouver et discuter avec moi sur Discord</p>
-            <a href="https://discord.com" target="_blank" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                xav5 sur Discord
-                <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                </svg>
-            </a>
-        </div>
     </div>
 </template>
